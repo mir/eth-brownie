8d11282ea02449b7d6ef6595141e2f891a502e0a
Marat Yuldashev <marat.spb@protonmail.com>
2022-04-06 20:37:59 +0300

    Finilized

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,14 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 import os
 
+def get_account():
+    if (network.show_active() == 'development'):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
 def deploy_contract():
-    # account = accounts.add(config["wallets"]["from_key"])
-    account = accounts[0]    
+    account = get_account()    
     simple_storage = SimpleStorage.deploy({"from": account})
     print(simple_storage)    
     stored_value = simple_storage.retrieve()
--- tests/test_simple_storage.py
+++ tests/test_simple_storage.py
@@ -5,3 +5,11 @@ def test_deploy():
     simple_storage = SimpleStorage.deploy({"from" : account})
     got_value = simple_storage.retrieve()
     assert got_value == 0
+
+def test_update_storage():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    TEST_VALUE = 15
+    simple_storage.store(TEST_VALUE, {"from" : account})
+    got_value = simple_storage.retrieve()
+    assert got_value == TEST_VALUE
